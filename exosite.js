0d3054d154a5320ca822fffe837df4f4449c7a1b
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-04-10 10:45:21 +0200

    init

---
+++ exosite.js
@@ -0,0 +1,34 @@
+module.exports = function (RED) {
+  "use strict";
+  var request = require('request');
+
+  function sendExosite(topic, payload, CIK){
+    var myUrl = "http://m2.exosite.com/onep:v1/stack/alias";
+
+    request.post({
+      headers : {
+        "X-Exosite-CIK" : CIK,
+        "Content-Type" : "application/x-www-form-urlencoded; charset=utf-8",
+        "Accept" : "application/xhtml+xml"
+      },
+      url : myUrl,
+      body : `${topic}=${payload}`
+    }, function(error, response, body) {
+      if(error) {
+        console.log("\n[Exosite] Error sending data\n");
+			}
+    });
+  }
+
+  function ExositeNode(n) {
+    RED.nodes.createNode(this,n);
+
+    this.on("input", function(msg) {
+      var topic = msg.topic||n.topic;
+      var payload = msg.payload;
+      var CIK = msg.CIK||n.CIK;
+      sendExosite(topic, payload, CIK);
+    });
+  }
+  RED.nodes.registerType("exosite", ExositeNode);
+}
